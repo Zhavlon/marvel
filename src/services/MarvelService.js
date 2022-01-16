b55daea76415dd3b14511aa20fa917697fb2fa06
Zhavlon <javlonadakhamov@gmail.com>
2022-01-16 23:44:54 +0600

    realized randomChar component and made transformData

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,12 +13,24 @@ class MarvelService {
 	}
 
 
-	getAllCharacters = () => {
-		return this.getResource(`${this._apiBase}characters?limit=9&offset=600&${this._apiKey}`)
+	getAllCharacters = async () => {
+		const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=250&${this._apiKey}`)
+		return res.data.results.map(this._transformCharacter)
 	}
 
-	getCharacter = () => {
-		return this.getResource(`${this._apiBase}characters/1011413?${this._apiKey}`)
+	getCharacter = async (id) => {
+		const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+		return this._transformCharacter(res.data.results[0])
+	}
+
+	_transformCharacter = (res) => {
+		return {
+			name: res.name,
+			description: res.description,
+			thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+			homepage: res.urls[0].url,
+			wiki: res.urls[1].url
+		}
 	}
 }
 
