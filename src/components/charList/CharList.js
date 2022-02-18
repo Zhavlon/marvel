16194cedee82d00a6b2a6b521d06b4ee95807cd2
Zhavlon <javlonadakhamov@gmail.com>
2022-02-18 23:08:01 +0600

    refactor randomChar using custom hook

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -8,123 +8,123 @@ import ErrorMessage from "../errorMessage/errorMessage";
 import './charList.scss';
 
 const CharList = ({onCharSelected}) => {
-    const [charList, setCharList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [newCharListLoading, setNewCharListLoading] = useState(false);
-    const [offset, setOffset] = useState(250);
-    const [charEnded, setCharEnded] = useState(false);
+	const [charList, setCharList] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [newCharListLoading, setNewCharListLoading] = useState(false);
+	const [offset, setOffset] = useState(250);
+	const [charEnded, setCharEnded] = useState(false);
 
-    const marvelService = new MarvelService()
+	const marvelService = new MarvelService()
 
-    useEffect(() => {
-        onRequest()
-    }, [])
+	useEffect(() => {
+		onRequest()
+	}, [])
 
-    useEffect(() => {
-        window.addEventListener('scroll', scrollItemsLoad)
-        return () => {
-            window.removeEventListener('scroll', scrollItemsLoad)
-        }
-    })
+	useEffect(() => {
+		window.addEventListener('scroll', scrollItemsLoad)
+		return () => {
+			window.removeEventListener('scroll', scrollItemsLoad)
+		}
+	})
 
-    const scrollItemsLoad = () => {
-        const page = document.documentElement
+	const scrollItemsLoad = () => {
+		const page = document.documentElement
 
-        if (page.clientHeight + page.scrollTop >= page.scrollHeight) {
-            onRequest(offset)
-        }
-    }
+		if (page.clientHeight + page.scrollTop >= page.scrollHeight) {
+			onRequest(offset)
+		}
+	}
 
-    const onRequest = (offset) => {
-        setNewCharListLoading(true)
-        marvelService.getAllCharacters(offset)
-            .then(onCharListLoaded)
-            .catch(onError)
-    }
+	const onRequest = (offset) => {
+		setNewCharListLoading(true)
+		marvelService.getAllCharacters(offset)
+			.then(onCharListLoaded)
+			.catch(onError)
+	}
 
-    const onError = () => {
-        setLoading(false)
-        setError(true)
-    }
+	const onError = () => {
+		setLoading(false)
+		setError(true)
+	}
 
-    const onCharListLoaded = (newCharList) => {
-        let ended = false
-        if (newCharList.length < 9) {
-            ended = true
-        }
+	const onCharListLoaded = (newCharList) => {
+		let ended = false
+		if (newCharList.length < 9) {
+			ended = true
+		}
 
-        setCharList(charList => ([...charList, ...newCharList]))
-        setLoading(false)
-        setNewCharListLoading(false)
-        setOffset(offset => offset + 9)
-        setCharEnded(ended)
-    }
+		setCharList(charList => ([...charList, ...newCharList]))
+		setLoading(false)
+		setNewCharListLoading(false)
+		setOffset(offset => offset + 9)
+		setCharEnded(ended)
+	}
 
-    const itemRefs = useRef([])
+	const itemRefs = useRef([])
 
-    const refFocus = (i) => {
-        itemRefs.current.forEach(item => {
-            item.classList.remove('char__item_selected')
-        })
+	const refFocus = (i) => {
+		itemRefs.current.forEach(item => {
+			item.classList.remove('char__item_selected')
+		})
 
-        itemRefs.current[i].classList.add('char__item_selected')
-        itemRefs.current[i].focus()
-    }
+		itemRefs.current[i].classList.add('char__item_selected')
+		itemRefs.current[i].focus()
+	}
 
-    const renderItems = (arr) => {
-        const items = arr.map(({thumbnail, name, id}, i) => {
+	const renderItems = (arr) => {
+		const items = arr.map(({thumbnail, name, id}, i) => {
 
-            const objectFit = thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'contain' : 'cover'
+			const objectFit = thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'contain' : 'cover'
 
-            return (
-                <li
-                    tabIndex={0}
-                    onClick={() => {
-                        onCharSelected(id)
-                        refFocus(i)
-                    }}
-                    ref={el => itemRefs.current[i] = el}
-                    key={id}
-                    className="char__item">
-                    <img src={thumbnail} style={{objectFit}} alt="abyss"/>
-                    <div className="char__name">{name}</div>
-                </li>
-            )
-        })
+			return (
+				<li
+					tabIndex={0}
+					onClick={() => {
+						onCharSelected(id)
+						refFocus(i)
+					}}
+					ref={el => itemRefs.current[i] = el}
+					key={id}
+					className="char__item">
+					<img src={thumbnail} style={{objectFit}} alt="abyss"/>
+					<div className="char__name">{name}</div>
+				</li>
+			)
+		})
 
-        return (
-            <ul className="char__grid">
-                {items}
-            </ul>
-        )
-    }
+		return (
+			<ul className="char__grid">
+				{items}
+			</ul>
+		)
+	}
 
-    const items = renderItems(charList)
+	const items = renderItems(charList)
 
-    const loader = loading ? <Loader/> : null
-    const errorMessage = error ? <ErrorMessage/> : null
-    const content = !(loading || error) ? items : null
+	const loader = loading ? <Loader/> : null
+	const errorMessage = error ? <ErrorMessage/> : null
+	const content = !(loading || error) ? items : null
 
-    return (
-        <div className="char__list">
-            {errorMessage}
-            {loader}
-            {content}
-            <button
-                style={{display: charEnded ? 'none' : 'block'}}
-                onClick={() => onRequest(offset)}
-                disabled={newCharListLoading}
-                className="button button__main button__long"
-            >
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+	return (
+		<div className="char__list">
+			{errorMessage}
+			{loader}
+			{content}
+			<button
+				style={{display: charEnded ? 'none' : 'block'}}
+				onClick={() => onRequest(offset)}
+				disabled={newCharListLoading}
+				className="button button__main button__long"
+			>
+				<div className="inner">load more</div>
+			</button>
+		</div>
+	)
 }
 
 CharList.propTypes = {
-    onCharSelected: PropTypes.func
+	onCharSelected: PropTypes.func
 }
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,35 +1,35 @@
 import {useHttp} from '../hooks/http.hook'
 
 const useMarvelService = () => {
-    const {loading, error, request} = useHttp()
+	const {loading, error, request, clearError} = useHttp()
 
-    const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-    const _apiKey = 'apikey=fb532fff92184ba4eac9f9a871e66ed5'
-    const _baseOffset = 250
+	const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+	const _apiKey = 'apikey=fb532fff92184ba4eac9f9a871e66ed5'
+	const _baseOffset = 250
 
-    const getAllCharacters = async (offset = _baseOffset) => {
-        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`)
-        return res.data.results.map(_transformCharacter)
-    }
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`)
+		return res.data.results.map(_transformCharacter)
+	}
 
-    const getCharacter = async (id) => {
-        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`)
-        return _transformCharacter(res.data.results[0])
-    }
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}characters/${id}?${_apiKey}`)
+		return _transformCharacter(res.data.results[0])
+	}
 
-    const _transformCharacter = (res) => {
-        return {
-            id: res.id,
-            name: res.name,
-            description: res.description ? `${res.description.slice(0, 200)}...` : 'This character has no description :)',
-            thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
-            homepage: res.urls[0].url,
-            wiki: res.urls[1].url,
-            comics: res.comics.items
-        }
-    }
+	const _transformCharacter = (res) => {
+		return {
+			id: res.id,
+			name: res.name,
+			description: res.description ? `${res.description.slice(0, 200)}...` : 'This character has no description :)',
+			thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+			homepage: res.urls[0].url,
+			wiki: res.urls[1].url,
+			comics: res.comics.items
+		}
+	}
 
-    return {loading, error, getAllCharacters, getCharacter}
+	return {loading, error, clearError, getAllCharacters, getCharacter}
 }
 
 export default useMarvelService
