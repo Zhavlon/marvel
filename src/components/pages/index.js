9638adc1f6b397becf3cac48608b24c076b83028
Zhavlon <javlonadakhamov@gmail.com>
2022-02-22 09:02:11 +0600

    specific router settings

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,6 @@
 import MainPage from './mainPage'
 import ComicsPage from "./comicsPage";
 import NoMatch from './404'
+import SingleComicPage from './singleComicPage'
 
-export {MainPage, ComicsPage, NoMatch}
+export {MainPage, ComicsPage, NoMatch, SingleComicPage}
